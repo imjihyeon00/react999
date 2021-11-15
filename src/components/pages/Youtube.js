2ba5b81ab65c6e6d53b308d0ad324c08532cb6de
imjihyeon00 <imjihyeon00@gmail.com>
2021-11-15 15:38:12 +0900

    react 수업 youtube api 서치
    env를 이용하여 api key 숨기기

--- src/components/pages/Youtube.js
+++ src/components/pages/Youtube.js
@@ -1,40 +1,77 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { useEffect, useState } from "react";
+// import {Link} from "react-router-dom";
 import Header from "../layouts/Header";
 import Footer from "../layouts/Footer";
 import Contents from "../layouts/Contents";
+import WrapTitle from "../layouts/WrapTitle";
+import ContInfo from "../info/ContInfo";
+import YoutubeList from "../info/YoutubeList";
+import YoutubeSearch from "../info/YoutubeSearch";
+
+require("dotenv").config();
+
+
 
 function Youtube(){
+    const [videos, setVideos] = useState([]);
+    const start = ()=>{
+        setTimeout(()=>{
+            // alert("ddd");
+        }, 2000);
+    };
+
+    
+
+    const search = (query)=>{
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+        
+        fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&maxResults=30&type=video`, requestOptions)
+        .then(response => response.json())
+        .then(result => setVideos(result.items))
+        .catch(error => console.log('error', error));
+    };
+
+    useEffect(()=>{
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        
+        fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=bts&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&maxResults=30&type=video`, requestOptions)
+        .then(response => response.json())
+        // .then(result => console.log(result.items))
+        .then(result => {
+            setVideos(result.items);
+            start();
+        })
+        .catch(error => console.log('error', error));
+    }, []);
+
     return (
         <div>
             <Header />
             <Contents>
                 <section id="youtubeCont">
                     <div className="container">
-                        <div className="wrap__title">
-                            <h2><strong>Youtube</strong><em>Reference</em></h2>
-                        </div>
+                        <WrapTitle text={["youtube", 'reference']} />
                         <div className="youtube__cont">
+                            <YoutubeSearch onSearch={search} />
                             <div className="youtube">
-                                
+                                <YoutubeList videos={videos} />
                             </div>
                         </div>
                     </div>
                 </section>
-
-                <section id="contactInfo">
-                    <div className="contact__info">
-                        <div>
-                            <p>Let’s create something new</p>
-                            <h3>LET`S Get IN<br />TOUCH</h3>
-                            <Link href="/contact" className="contact">Contact Me</Link>
-                        </div>
-                    </div>
-                </section>
-            </Contents>    
+                <ContInfo />
+            </Contents>
             <Footer />
         </div>
-    )
+    );
 }
 
 export default Youtube;
